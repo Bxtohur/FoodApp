38138577a12b2b335c11a715db9b30177bf7ab4f
Bxtohur <bitohur@gmail.com>
2023-10-08 18:29:31 +0700

    clear typo

--- app/src/main/java/com/bitohur/foodapp/utils/GenericViewModelFactory.kt
+++ app/src/main/java/com/bitohur/foodapp/utils/GenericViewModelFactory.kt
@@ -1,4 +1,4 @@
-package com.catnip.egroceries.utils
+package com.bitohur.foodapp.utils
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
--- app/src/main/java/com/bitohur/foodapp/utils/GridSpacingItemDecoration.kt
+++ app/src/main/java/com/bitohur/foodapp/utils/GridSpacingItemDecoration.kt
@@ -1,4 +1,4 @@
-package com.catnip.egroceries.utils
+package com.bitohur.foodapp.utils
 
 import android.graphics.Rect
 import android.view.View
